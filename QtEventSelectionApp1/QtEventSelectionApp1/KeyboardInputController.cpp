b0e8224243cab0456af8f0fab011e3b7ddd57a4c
Xydrel <jasonh78@gmail.com>
2019-08-07 10:27:37 -0700

    FIX: Now able to handle arrow key events as desired

--- QtEventSelectionApp1/QtEventSelectionApp1/KeyboardInputController.cpp
+++ QtEventSelectionApp1/QtEventSelectionApp1/KeyboardInputController.cpp
@@ -9,29 +9,32 @@ KeyboardInputController::KeyboardInputController(QObject* parent)
 
 bool KeyboardInputController::eventFilter(QObject* watched, QEvent* event)
 {
-	if (event->type() == QEvent::KeyPress)
-	{
-		QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
-		switch (keyEvent->key())
+	//if (event->type() == QEvent::KeyPress)
+	//{
+		QKeyEvent* keyEvent = dynamic_cast<QKeyEvent*>(event);
+		if (keyEvent != nullptr)
 		{
-			case Qt::Key_Escape:
+			switch (keyEvent->key())
 			{
-				QApplication::quit();
-				return true;
+				case Qt::Key_Escape:
+				{
+					QApplication::quit();
+					return true;
+				}
+				case Qt::Key_Left:
+				{
+					//todo: call chain to move the selection left
+					return true;
+				}			
+				case Qt::Key_Right:
+				{
+					//todo: call chain to move the selection right
+					return true;
+				}
+				default:
+					return false;
 			}
-			case Qt::Key_Left:
-			{
-				//todo: call chain to move the selection left
-				return true;
-			}			
-			case Qt::Key_Right:
-			{
-				//todo: call chain to move the selection right
-				return true;
-			}
-			default:
-				return false;
 		}
-	}
+	//}
 	return false;
 }
